7c2f16df56b3a7d2f68b9b1708fbd2bf95cb4458
stefangospic <stefan.gospic.1@gmail.com>
2026-01-04 19:13:00 +0100

    Seedovani podaci

--- BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
@@ -43,5 +43,250 @@ public class AppDbContext:DbContext
             .WithMany(publisher => publisher.Books)
             .HasForeignKey(book => book.PublisherId)
             .OnDelete(DeleteBehavior.Restrict);
+        
+        // ========== SEED PODACI ==========
+        
+        // 5 Autora
+        modelBuilder.Entity<Author>().HasData(
+            new Author 
+            { 
+                Id = 1, 
+                FullName = "Ivo Andrić", 
+                Biography = "Jugoslovenski pisac, dobitnik Nobelove nagrade za književnost 1961. godine. Najpoznatiji po romanima o Bosni.", 
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1892, 10, 9), DateTimeKind.Utc)
+            },
+            new Author 
+            { 
+                Id = 2, 
+                FullName = "Meša Selimović", 
+                Biography = "Bosanskohercegovački i jugoslovenski pisac. Najpoznatiji po romanu 'Derviš i smrt'.", 
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1910, 4, 26), DateTimeKind.Utc)
+            },
+            new Author 
+            { 
+                Id = 3, 
+                FullName = "Dobrica Ćosić", 
+                Biography = "Srpski romanopisac i političar, autor velikih romanesknih ciklusa o srpskoj istoriji.", 
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1921, 12, 29), DateTimeKind.Utc)
+            },
+            new Author 
+            { 
+                Id = 4, 
+                FullName = "Milorad Pavić", 
+                Biography = "Srpski pesnik, prozni pisac i književni istoričar. Najpoznatiji po romanu 'Hazarski rečnik'.", 
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1929, 10, 15), DateTimeKind.Utc)
+            },
+            new Author 
+            { 
+                Id = 5, 
+                FullName = "Danilo Kiš", 
+                Biography = "Jugoslovenski pisac jevrejskog porekla, jedan od najznačajnijih autora 20. veka.", 
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1935, 2, 22), DateTimeKind.Utc)
+            }
+        );
+
+        // 3 Izdavača
+        modelBuilder.Entity<Publisher>().HasData(
+            new Publisher 
+            { 
+                Id = 1, 
+                Name = "Prosveta", 
+                Address = "Terazije 16, Beograd", 
+                Website = "https://www.prosveta.rs" 
+            },
+            new Publisher 
+            { 
+                Id = 2, 
+                Name = "Laguna", 
+                Address = "Resavska 33, Beograd", 
+                Website = "https://www.laguna.rs" 
+            },
+            new Publisher 
+            { 
+                Id = 3, 
+                Name = "Vulkan izdavaštvo", 
+                Address = "Makedonska 30, Beograd", 
+                Website = "https://www.vulkanizdavastvo.rs" 
+            }
+        );
+
+        // 12 Knjiga
+        modelBuilder.Entity<Book>().HasData(
+            new Book 
+            { 
+                Id = 1, 
+                Title = "Na Drini ćuprija", 
+                PageCount = 352, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1945, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00001-1", 
+                AuthorId = 1, 
+                PublisherId = 1 
+            },
+            new Book 
+            { 
+                Id = 2, 
+                Title = "Travnička hronika", 
+                PageCount = 288, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1942, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00002-8", 
+                AuthorId = 1, 
+                PublisherId = 1 
+            },
+            new Book 
+            { 
+                Id = 3, 
+                Title = "Prokleta avlija", 
+                PageCount = 176, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1954, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00003-5", 
+                AuthorId = 1, 
+                PublisherId = 2 
+            },
+            new Book 
+            { 
+                Id = 4, 
+                Title = "Derviš i smrt", 
+                PageCount = 368, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1966, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00004-2", 
+                AuthorId = 2, 
+                PublisherId = 1 
+            },
+            new Book 
+            { 
+                Id = 5, 
+                Title = "Tvrđava", 
+                PageCount = 312, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00005-9", 
+                AuthorId = 2, 
+                PublisherId = 2 
+            },
+            new Book 
+            { 
+                Id = 6, 
+                Title = "Koreni", 
+                PageCount = 624, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1954, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00006-6", 
+                AuthorId = 3, 
+                PublisherId = 1 
+            },
+            new Book 
+            { 
+                Id = 7, 
+                Title = "Deobe", 
+                PageCount = 544, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1961, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00007-3", 
+                AuthorId = 3, 
+                PublisherId = 3 
+            },
+            new Book 
+            { 
+                Id = 8, 
+                Title = "Vreme smrti", 
+                PageCount = 1200, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1972, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00008-0", 
+                AuthorId = 3, 
+                PublisherId = 1 
+            },
+            new Book 
+            { 
+                Id = 9, 
+                Title = "Hazarski rečnik", 
+                PageCount = 336, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1984, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00009-7", 
+                AuthorId = 4, 
+                PublisherId = 2 
+            },
+            new Book 
+            { 
+                Id = 10, 
+                Title = "Predeo slikan čajem", 
+                PageCount = 192, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1988, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00010-3", 
+                AuthorId = 4, 
+                PublisherId = 2 
+            },
+            new Book 
+            { 
+                Id = 11, 
+                Title = "Bašta, pepeo", 
+                PageCount = 264, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1965, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00011-0", 
+                AuthorId = 5, 
+                PublisherId = 3 
+            },
+            new Book 
+            { 
+                Id = 12, 
+                Title = "Grobnica za Borisa Davidoviča", 
+                PageCount = 144, 
+                PublishedDate = DateTime.SpecifyKind(new DateTime(1976, 1, 1), DateTimeKind.Utc),
+                ISBN = "978-86-01-00012-7", 
+                AuthorId = 5, 
+                PublisherId = 3 
+            }
+        );
+
+        // 4 Nagrade
+        modelBuilder.Entity<Award>().HasData(
+            new Award 
+            { 
+                Id = 1, 
+                Name = "Nobelova nagrada za književnost", 
+                Description = "Najpoznatija književna nagrada na svetu, dodeljena za izuzetan doprinos svetskoj književnosti.", 
+                YearOfFirstAssignment = 1901 
+            },
+            new Award 
+            { 
+                Id = 2, 
+                Name = "NIN nagrada", 
+                Description = "Prestižna srpska književna nagrada za najbolji roman godine, dodeljena od strane lista NIN.", 
+                YearOfFirstAssignment = 1954 
+            },
+            new Award 
+            { 
+                Id = 3, 
+                Name = "Andrićeva nagrada", 
+                Description = "Srpska književna nagrada za najbolju pripovetku ili roman.", 
+                YearOfFirstAssignment = 1981 
+            },
+            new Award 
+            { 
+                Id = 4, 
+                Name = "Evropska književna nagrada", 
+                Description = "Međunarodna književna nagrada za izuzetan doprinos evropskoj književnosti.", 
+                YearOfFirstAssignment = 1985 
+            }
+        );
+
+        // 15 podataka u tabeli poveznici (AuthorAwardBridge)
+        modelBuilder.Entity<AuthorAward>().HasData(
+            new AuthorAward { AuthorId = 1, AwardId = 1, YearAwarded = 1961 },
+            new AuthorAward { AuthorId = 1, AwardId = 2, YearAwarded = 1954 },
+            new AuthorAward { AuthorId = 1, AwardId = 3, YearAwarded = 1982 },
+            
+            new AuthorAward { AuthorId = 2, AwardId = 2, YearAwarded = 1966 },
+            new AuthorAward { AuthorId = 2, AwardId = 3, YearAwarded = 1985 },
+            new AuthorAward { AuthorId = 2, AwardId = 4, YearAwarded = 1990 },
+            
+            new AuthorAward { AuthorId = 3, AwardId = 2, YearAwarded = 1962 },
+            new AuthorAward { AuthorId = 3, AwardId = 3, YearAwarded = 1981 },
+            new AuthorAward { AuthorId = 3, AwardId = 4, YearAwarded = 1988 },
+            
+            new AuthorAward { AuthorId = 4, AwardId = 2, YearAwarded = 1985 },
+            new AuthorAward { AuthorId = 4, AwardId = 3, YearAwarded = 1990 },
+            new AuthorAward { AuthorId = 4, AwardId = 4, YearAwarded = 1991 },
+            
+            new AuthorAward { AuthorId = 5, AwardId = 2, YearAwarded = 1973 },
+            new AuthorAward { AuthorId = 5, AwardId = 3, YearAwarded = 1987 },
+            new AuthorAward { AuthorId = 5, AwardId = 4, YearAwarded = 1989 }
+        );
     }
 }
---
+++ BookstoreApplication/BookstoreApplication/Migrations/20260104180704_v3.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookstoreApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Biography", "Birthday", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "Jugoslovenski pisac, dobitnik Nobelove nagrade za književnost 1961. godine. Najpoznatiji po romanima o Bosni.", new DateTime(1892, 10, 9, 0, 0, 0, 0, DateTimeKind.Utc), "Ivo Andrić" },
+                    { 2, "Bosanskohercegovački i jugoslovenski pisac. Najpoznatiji po romanu 'Derviš i smrt'.", new DateTime(1910, 4, 26, 0, 0, 0, 0, DateTimeKind.Utc), "Meša Selimović" },
+                    { 3, "Srpski romanopisac i političar, autor velikih romanesknih ciklusa o srpskoj istoriji.", new DateTime(1921, 12, 29, 0, 0, 0, 0, DateTimeKind.Utc), "Dobrica Ćosić" },
+                    { 4, "Srpski pesnik, prozni pisac i književni istoričar. Najpoznatiji po romanu 'Hazarski rečnik'.", new DateTime(1929, 10, 15, 0, 0, 0, 0, DateTimeKind.Utc), "Milorad Pavić" },
+                    { 5, "Jugoslovenski pisac jevrejskog porekla, jedan od najznačajnijih autora 20. veka.", new DateTime(1935, 2, 22, 0, 0, 0, 0, DateTimeKind.Utc), "Danilo Kiš" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Awards",
+                columns: new[] { "Id", "Description", "Name", "YearOfFirstAssignment" },
+                values: new object[,]
+                {
+                    { 1, "Najpoznatija književna nagrada na svetu, dodeljena za izuzetan doprinos svetskoj književnosti.", "Nobelova nagrada za književnost", 1901 },
+                    { 2, "Prestižna srpska književna nagrada za najbolji roman godine, dodeljena od strane lista NIN.", "NIN nagrada", 1954 },
+                    { 3, "Srpska književna nagrada za najbolju pripovetku ili roman.", "Andrićeva nagrada", 1981 },
+                    { 4, "Međunarodna književna nagrada za izuzetan doprinos evropskoj književnosti.", "Evropska književna nagrada", 1985 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Id", "Address", "Name", "Website" },
+                values: new object[,]
+                {
+                    { 1, "Terazije 16, Beograd", "Prosveta", "https://www.prosveta.rs" },
+                    { 2, "Resavska 33, Beograd", "Laguna", "https://www.laguna.rs" },
+                    { 3, "Makedonska 30, Beograd", "Vulkan izdavaštvo", "https://www.vulkanizdavastvo.rs" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AuthorAwardBridge",
+                columns: new[] { "AuthorId", "AwardId", "YearAwarded" },
+                values: new object[,]
+                {
+                    { 1, 1, 1961 },
+                    { 1, 2, 1954 },
+                    { 1, 3, 1982 },
+                    { 2, 2, 1966 },
+                    { 2, 3, 1985 },
+                    { 2, 4, 1990 },
+                    { 3, 2, 1962 },
+                    { 3, 3, 1981 },
+                    { 3, 4, 1988 },
+                    { 4, 2, 1985 },
+                    { 4, 3, 1990 },
+                    { 4, 4, 1991 },
+                    { 5, 2, 1973 },
+                    { 5, 3, 1987 },
+                    { 5, 4, 1989 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "ISBN", "PageCount", "PublishedDate", "PublisherId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "978-86-01-00001-1", 352, new DateTime(1945, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 1, "Na Drini ćuprija" },
+                    { 2, 1, "978-86-01-00002-8", 288, new DateTime(1942, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 1, "Travnička hronika" },
+                    { 3, 1, "978-86-01-00003-5", 176, new DateTime(1954, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 2, "Prokleta avlija" },
+                    { 4, 2, "978-86-01-00004-2", 368, new DateTime(1966, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 1, "Derviš i smrt" },
+                    { 5, 2, "978-86-01-00005-9", 312, new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 2, "Tvrđava" },
+                    { 6, 3, "978-86-01-00006-6", 624, new DateTime(1954, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 1, "Koreni" },
+                    { 7, 3, "978-86-01-00007-3", 544, new DateTime(1961, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 3, "Deobe" },
+                    { 8, 3, "978-86-01-00008-0", 1200, new DateTime(1972, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 1, "Vreme smrti" },
+                    { 9, 4, "978-86-01-00009-7", 336, new DateTime(1984, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 2, "Hazarski rečnik" },
+                    { 10, 4, "978-86-01-00010-3", 192, new DateTime(1988, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 2, "Predeo slikan čajem" },
+                    { 11, 5, "978-86-01-00011-0", 264, new DateTime(1965, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 3, "Bašta, pepeo" },
+                    { 12, 5, "978-86-01-00012-7", 144, new DateTime(1976, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 3, "Grobnica za Borisa Davidoviča" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 5, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 5, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorAwardBridge",
+                keyColumns: new[] { "AuthorId", "AwardId" },
+                keyValues: new object[] { 5, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Awards",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
