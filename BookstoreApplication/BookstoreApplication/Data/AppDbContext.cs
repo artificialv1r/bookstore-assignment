47f42618e48d7b03aad0ed2e1b293c7354648662
stefangospic <stefan.gospic.1@gmail.com>
2026-01-04 15:57:15 +0100

    Uvedene klase potrebne da se modeluje nagrada i njena veza sa autorom. Kreirana migracija.

--- BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
@@ -11,4 +11,12 @@ public class AppDbContext:DbContext
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
     public DbSet<Publisher> Publishers { get; set; }
+    public DbSet<Award> Awards { get; set; }
+    public DbSet<AuthorAward> AuthorAwards { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AuthorAward>()
+            .HasKey(a => new { a.AuthorId, a.AwardId });
+    }
 }
---
+++ BookstoreApplication/BookstoreApplication/Models/Award.cs
@@ -0,0 +1,11 @@
+namespace BookstoreApplication.Models;
+
+public class Award
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int YearOfFirstAssignment { get; set; }
+    
+    public ICollection<AuthorAward> AuthorAwards { get; set; } 
+}
